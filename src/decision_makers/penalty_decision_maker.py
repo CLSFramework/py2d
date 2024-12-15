9786031ade27fb4a7bd485b64930ae4ab531747b
ErfanFathi <fathye897@gmail.com>
2024-12-15 05:28:29 -0800

    feat: refine `penalty kick` logic for kicker and goalie
    - Designed penalty kick behavior in `BhvStarterPenalty`:
      - Added kicker setup, readiness, and execution methods.
      - Implemented goalie actions: setup, waiting, and responses.
      - Enhanced decision-making for `dribbling` and `shooting`.
      - Improved goalie positioning based on ball dynamics.
      - Included support for various penalty game modes.

--- src/decision_makers/penalty_decision_maker.py
+++ src/decision_makers/penalty_decision_maker.py
@@ -1,14 +1,14 @@
 from src.interfaces.IDecisionMaker import IDecisionMaker
 from src.interfaces.IAgent import IAgent
 from service_pb2 import *
-from src.behaviors.bhv_penalty import BhvPenalty
+# from src.behaviors.bhv_penalty import BhvPenalty
 from src.behaviors.bhv_starter_penalty import BhvStarterPenalty
 
 
 class PenaltyDecisionMaker(IDecisionMaker):
     def __init__(self):
-        self.bhv_penalty = BhvPenalty()
-        # self.bhv_penalty = BhvStarterPenalty()
+        #self.bhv_penalty = BhvPenalty()
+        self.bhv_penalty = BhvStarterPenalty()
     
     def make_decision(self, agent: IAgent):
         agent.logger.debug("PenaltyDecisionMaker.make_decision")
